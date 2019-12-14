80ae4b04ea079dcf724a3b382ab146b4cbe598f8
dd_minaev <dd_minaev@bingo-boom.com>
2019-12-14 17:13:46 +0300

    disabled create img

--- app/middlewares/create.js
+++ app/middlewares/create.js
@@ -4,6 +4,8 @@ module.exports = async (req, res, next) => {
     console.log('Call create middleware');
     try {
         const params = req.body;
+        // @todo ТОЛЬКО ДЛЯ ТОГО, ЧТОБЫ НЕ ЗАБИЛИ ВСЮ БАЗУ ;)
+        return next({imageId: null});
         const response = await storage.one(req.sqlQueries.create, {params});
         next(response);
     } catch (error) {
